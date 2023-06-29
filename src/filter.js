007647139e2679ce358200e9001f02d3e86a272c
Archik-K <ar.kryatova@yandex.ru>
2023-06-29 16:42:11 +0300

    changing the photo in daria's card

--- src/filter.js
+++ src/filter.js
@@ -22,8 +22,7 @@ function searchResult(element) {
 }
 
 document.addEventListener("DOMContentLoaded", async function () {
-	let url =
-		"https://raw.githubusercontent.com/elena-kundera/Hakaton/main/src/nanies.json";
+	let url = "src/nanies.json";
 	let response = await fetch(url);
 	cards = await response.json();
 	firstCards = cards;
